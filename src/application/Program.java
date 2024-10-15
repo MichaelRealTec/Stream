096948a31962dd720f8249e2767848f20d27a48c
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-10-15 20:52:54 -0300

    exercício de Stream resolvido

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,8 +15,19 @@ public class Program {
 		// Criar um Stream inteiro
 		//Stream<Integer> st1 = list.stream();
 		Stream<Integer> st1 = list.stream().map(x -> x * 10);// map aplica uma função a cada elemento da minha Stream.
-		
 		System.out.println(Arrays.toString(st1.toArray()));
+		
+		// Criar segunda Stream formato (String)
+		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
+		
+		// Criar uma Stream para calcular os 10 primeiros elementos
+		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+		System.out.println(Arrays.toString(st3.limit(10).toArray())); // limite de 10 elementos
+	
+		// Criar uma Stream para calcular a sequencia de Fibonacci limitando a 20 elementos
+		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0] + p[1]}).map(p -> p[0]);
+		System.out.println(Arrays.toString(st4.limit(20).toArray()));
 	}
 
 }
